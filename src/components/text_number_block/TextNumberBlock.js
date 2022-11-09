8e9113524681e538684a79bf81c2701e843f8791
benedekhalaj <bendihalaj@gmail.com>
2022-11-09 20:50:59 +0100

    feat: add text parameter to TextNumberBlock component

--- src/components/text_number_block/TextNumberBlock.js
+++ src/components/text_number_block/TextNumberBlock.js
@@ -1,13 +1,14 @@
 import {Card, TextField} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import PropTypes from "prop-types";
 
 
-const TextNumberBlock = () => {
+const TextNumberBlock = (props) => {
     return (
         <Card>
             <Grid2 container spacing={2} padding={"1rem"}>
                 <Grid2 xs={8}>
-                    <p style={{fontWeight: "bold"}}>Terraforming Rate</p>
+                    <p style={{fontWeight: "bold"}}>{props.text}</p>
                 </Grid2>
                 <Grid2 xs={4}>
                     <TextField type={"number"} defaultValue={0}/>
@@ -15,6 +16,10 @@ const TextNumberBlock = () => {
             </Grid2>
         </Card>
     );
+}
+
+TextNumberBlock.propTypes = {
+    text: PropTypes.string.isRequired
 }
 
 export default TextNumberBlock;
