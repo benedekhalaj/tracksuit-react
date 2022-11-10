2d7b65987576ceb9c4d3df0ca27bdb1e90cb80bd
benedekhalaj <bendihalaj@gmail.com>
2022-11-10 22:44:52 +0100

    feat: add simple input to TextNumberBlock component

--- src/components/text_number_block/TextNumberBlock.js
+++ src/components/text_number_block/TextNumberBlock.js
@@ -1,17 +1,29 @@
 import {Card, TextField} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import PropTypes from "prop-types";
+import {isBrowser} from "react-device-detect";
 
 
 const TextNumberBlock = (props) => {
     return (
         <Card>
-            <Grid2 container spacing={2} padding={"1rem"}>
+            <Grid2 justifyItems={"center"} alignContent={"center"} container spacing={2} padding={"1%"} lineHeight={"0%"}>
                 <Grid2 xs={8}>
                     <p style={{fontWeight: "bold"}}>{props.text}</p>
                 </Grid2>
                 <Grid2 xs={4}>
-                    <TextField type={"number"} defaultValue={0}/>
+                    <input
+                        style={{
+                            width: "100%",
+                            height: "35px",
+                            padding: "2px",
+                            borderRadius: "2px",
+                            border: "1px lightgray solid",
+                            font: "inherit"
+                    }}
+                        type={"number"}
+                        placeholder={(isBrowser) ? "points" : "points"}
+                    />
                 </Grid2>
             </Grid2>
         </Card>
