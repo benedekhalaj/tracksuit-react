d09b0142054e5380ceba53acd092679cd0d5bd26
benedekhalaj <bendihalaj@gmail.com>
2022-11-17 11:37:54 +0100

    chore: add CategoryService and PlayerService

---
+++ src/services/CategoryService.ts
@@ -0,0 +1,10 @@
+const getCategories = (): string[] => {
+    return ["Terraforming Rate", "Milestone Points", "Award Points", "Greeneries",
+        "Greeneries around Cities", "Victory Points on Cards", "Money"];
+}
+
+const methods = {
+    getCategories: getCategories
+};
+
+export default methods;
---
+++ src/services/PlayerService.ts
@@ -0,0 +1,11 @@
+const getPlayers = (): string[] => {
+    return ["Juhász Zsuzsanna Ilona", "Pozsonyi Gábor János", "Halaj Léna Frida",
+        "Sági Viktor Ferenc", "Dudaskó Bendegúz Attila", "Turcsányi Vivien Erzsébet",
+        "Halaj Benedek Herbert", "Körmendi Zsófia Izolda"];
+}
+
+const methods = {
+    getPlayers: getPlayers
+};
+
+export default methods;
