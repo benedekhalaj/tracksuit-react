e0fc516f87eae5c7e0b50c02dd95145c33fb7b27
benedekhalaj <bendihalaj@gmail.com>
2022-11-25 14:26:14 +0100

    chore: add Scores with Swiper

--- src/components/scores/Scores.tsx
+++ src/components/scores/Scores.tsx
@@ -1,27 +1,41 @@
 import {FunctionComponent, useEffect, useState} from "react";
 import {Paper} from "@mui/material";
-import PlayerScore from "../player_score/PlayerScore";
+import PlayerScoreComponent from "../player_score/PlayerScoreComponent";
 import MatchScore from "../../interfaces/match-score";
+import scoreService from "../../services/score-service";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation, Pagination} from "swiper";
+import {isBrowser} from "react-device-detect";
 
 const Scores: FunctionComponent = () => {
 
     const [matchScore, setMatchScore] = useState<MatchScore>({matchName: "", playerScores: []});
 
     useEffect(() => {
-        fetch("http://localhost:8080/api/v0/scores")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setMatchScore(data);
-            });
+        scoreService.getScores().then(scores => {
+            console.log(scores);
+            setMatchScore(scores);
+        });
     }, [setMatchScore]);
-
-
 
     return (
         <Paper>
-            <h1>Scores</h1>
-            {matchScore.playerScores.map(playerScore => <PlayerScore key={playerScore.playerName} playerScore={playerScore} />)}
+            <h2>Scores</h2>
+            <Swiper
+                style={{backgroundColor: "lightblue", userSelect: "none", borderRadius: "8px"}}
+                modules={[Pagination, Navigation]}
+                pagination={{clickable: true}}
+                navigation
+                slidesPerView={(isBrowser) ? 3 : 1}
+                spaceBetween={20}
+            >
+                {matchScore.playerScores.map((playerScore, index) =>
+                    <SwiperSlide key={"SwiperSlide-" + index}>
+                        <PlayerScoreComponent key={playerScore.playerName} playerScore={playerScore}/>
+                    </SwiperSlide>
+                )}
+            </Swiper>
+
         </Paper>
     );
 }
