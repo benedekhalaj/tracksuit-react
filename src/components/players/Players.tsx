032acc8651b1d1eadbfd3c2f37cf0033abba4549
benedekhalaj <bendihalaj@gmail.com>
2022-11-26 00:59:47 +0100

    chore: add createPlayer and getNewPlayerData to Players component

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -2,9 +2,26 @@ import {FunctionComponent, useEffect, useState} from "react";
 import PlayerModel from "../../interfaces/player-model";
 import playerService from "../../services/player-service";
 import Player from "../player/Player";
+import {Button} from "@mui/material";
+import player from "../player/Player";
 
 const Players: FunctionComponent = () => {
     const [players, setPlayers] = useState<PlayerModel[]>([]);
+
+    const getNewPlayerData = () => {
+        const playerName: string | null = prompt("Enter Player Name");
+        const appUserId: number = 1;
+        if (playerName !== null) {
+            createPlayer({
+                name: playerName,
+                appUser: {id: appUserId}
+            })
+        }
+    }
+
+    const createPlayer = (playerModel: PlayerModel) => {
+        playerService.createPlayer(playerModel).then(player => setPlayers([player, ...players]))
+    }
 
     const deletePlayer = (id: number): void => {
         const newPlayers: PlayerModel[] = players.filter(player => player.id !== id);
@@ -19,6 +36,7 @@ const Players: FunctionComponent = () => {
     return (
         <div>
             <h1>PLAYERS</h1>
+            <Button onClick={() => getNewPlayerData()}>Add new Player</Button>
             {players.map(player => <Player key={player.id} player={player} onDelete={(deletePlayer)}></Player>)}
         </div>
     );
