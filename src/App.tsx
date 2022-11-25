e2371fa5a4b01450f520b0e8bf2c3005f22122fd
benedekhalaj <bendihalaj@gmail.com>
2022-11-25 14:26:56 +0100

    feat: add Scores component to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,16 @@ import './App.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import CardsByCategory from "./components/cards_by_category/CardsByCategory";
-import PlayerService from "./services/player-service";
-import CategoryService from "./services/category-service";
 import {FunctionComponent} from "react";
+import Scores from "./components/scores/Scores";
 
 const App: FunctionComponent = () => {
-
-    const categories = CategoryService.getCategories();
-    const players = PlayerService.getPlayers();
 
     return (
         <Container style={{backgroundColor: "whitesmoke", padding: "25px"}}>
             <h1>TrackSuit</h1>
             <CssBaseline/>
-            <CardsByCategory categories={categories} players={players} />
+            <Scores />
         </Container>
     );
 }
